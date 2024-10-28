6697ceef82b3c6715571d866f83cdfe8569f87b7
Szymon Jakóbczak <hdwrdev@gmail.com>
2024-10-28 12:41:24 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using SReaderAPI;
+
+namespace ReadTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SReader reader = null;
+            try
+            {
+
+                reader = SReader.Create("tcp://192.168.1.136"); //tcp connect
+
+                reader.Connect();
+
+                var info = reader.GetReaderInfo();
+                if (info.getCurrentRegion() != (int)SReader.Region.EU)
+                {
+                    reader.SetRegion(SReader.Region.EU);
+                }
+                if (info.Power != 26)
+                    reader.SetReaderPower(26);
+
+                reader.SetBuzzer(1);
+
+                reader.TagRead += TagRead;
+
+                Gen2.InventryValue value = new Gen2.InventryValue(4, 1);
+                while (true)
+                {
+                    reader.Inventry(value, null);
+                }
+            }
+            catch (ReaderException ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                reader.ShutDown();
+            }
+        }
+
+        static public void TagRead(Object sender, TagReadDataEventArgs e)
+        {
+            string epc = e.TagData.EpcString;
+            int ant = e.TagData.Ant;
+            int rssi = e.TagData.Rssi;
+            Console.WriteLine("epc:" + epc + " ant:" + ant + " rssi:" + rssi);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Przyk³ad odczytu kodów RFID z wykorzystaniem czytnika HDWR HD-RD06-UHF
+## Opis projektu
+Ten projekt demonstruje sposób odczytu kodów RFID za pomoc¹ czytnika [HDWR HD-RD06-UHF](https://hdwr.pl/pl/p/Czytnik-z-antena-RFID%2C-odczyt-tagow-o-czestotliwosci-860-960-MHz%2C-HDWR-HD-RD06-UHF/1935). Urz¹dzenie to umo¿liwia odczyt tagów RFID o czêstotliwoœci 860-960 MHz.
+
+## Wymagania
+- Czytnik RFID [HDWR HD-RD06-UHF](https://hdwr.pl/pl/p/Czytnik-z-antena-RFID%2C-odczyt-tagow-o-czestotliwosci-860-960-MHz%2C-HDWR-HD-RD06-UHF/1935)
+- Komputer z systemem Windows
+- Biblioteka `SReaderAPI.dll` do obs³ugi urz¹dzenia
